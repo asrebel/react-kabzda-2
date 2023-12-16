9a9c145a3d5da70b5d3c58dba2d36eab6ac6d47e
asrebel <asrebel777@gmail.com>
2023-12-16 17:35:03 +0500

    tasks 00-03 are done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <PageTitle title = {'This is an App Component!'}/>
+            <PageTitle title = {'Yabadabado'}/>
+
+            Article 1
+            <Rating value = {3}/>
+            <Accordion title = {'Menu'}/>
+            <Accordion title = {'Menu123'}/>
+
+            Article 2
+            <Rating value = {4}/>
+
+        </div>
+    );
 }
+
+const PageTitle = (props:any) => {
+    return (
+        <h1>{props.title}</h1>
+    )
+}
+
 
 export default App;
